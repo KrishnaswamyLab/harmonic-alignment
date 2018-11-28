916290a93432b145beb852fcd48ac77ee1248e84
Jay Stanley <jay.s.stanley.3@gmail.com>
2018-11-28 15:42:13 -0500

    q

---
+++ python/utils.py
@@ -0,0 +1,22 @@
+
+def _check_and_transpose(ref, inData):
+    if ref not in inData.shape:
+        return False
+    else:
+        if inData.ndim < 2:
+            inData = inData[:, np.newaxis]
+        if inData.shape[0] == ref:
+            return inData
+        else:
+            pos = inData.shape.index(ref)
+            return np.swapaxes(inData, 0, pos)
+
+
+def _ispos(x):
+    return x > 0
+
+
+def _inposrng(x, y):
+    return _ispos(x) and x < y
+
+
