0b35af8650922d4fde9750b315cdb1c418aa8d2d
Jay Stanley <jay.s.stanley.3@gmail.com>
2018-11-28 15:35:10 -0500

    more diffusion wavelets updates, added a wavelet sueprclass

--- python/harmonicalignment.py
+++ python/harmonicalignment.py
@@ -1,23 +1,82 @@
 import numpy as np
 from scipy.sparse.linalg import eigs as eigs
 import warnings
+from abc import ABCMeta, abstractmethod
+from functools import partial
 
 def _check_and_transpose(ref, inData):
     if ref not in inData.shape:
         return False
     else:
-        if inData.ndim<2:
-            inData = inData[:,np.newaxis]
+        if inData.ndim < 2:
+            inData = inData[:, np.newaxis]
         if inData.shape[0] == ref:
             return inData
         else:
             pos = inData.shape.index(ref)
-            return np.swapaxes(inData, 0,pos)
+            return np.swapaxes(inData, 0, pos)
 
 
-class DiffusionWavelets(object):
-    def __init__(self, A, frames="tight", beta=None):
-        self.N = A.shape[0]
+def _ispos(x):
+    return x > 0
+
+
+def _inposrng(x, y):
+    return _ispos(x) and x < y
+
+
+class Wavelet(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, N, numframes, transform_spectrum):
+        self.numframes = numframes
+        self.N = N
+        self.spectral_representation = [None for x in range(numframes)]
+        self.T = None
+        self.W = None
+
+    def __getitem__(self, j):
+
+        if _inposrng(j, self.numframes):
+            return self.W[:, j * self.N:(j + 1) * self.N]
+        else:
+            raise KeyError("Attempted to slice a frame that does not exist.")
+
+    def evaluate_spectral(self, x=None, transform_spectrum="normalized"):
+        if x is None:
+            x = np.linspace(0, 2, 1000)
+        output = np.zeros((self.numframes, x.shape[0]))
+        for j in range(self.numframes):
+            output[j, :] = self.evaluate_frame_spectral(j, x,transform_spectrum)
+        return output
+
+    @abstractmethod
+    def _fouriermapping(self, x, domain, direction="forward"):
+        raise NotImplementedError("Must override _fouriermapping")
+
+    def evaluate_frame_spectral(self, j, x=None, transform_spectrum="normalized"):
+        if _inposrng(j, self.numframes):
+            x = self._fouriermapping(x, transform_spectrum, "forward")
+            return self._spectral_representation[j](x)
+
+    def transform(self, x, j=None):
+        x = _check_and_transpose(self.N, x)
+        if j is None:
+            return np.matmul(self.W.T, x)
+        else:
+            return np.matmul(self[j].T, x)
+
+    def power_density(self, x, p=2):
+        x = _check_and_transpose(self.N, x)
+        x_density = np.zeros((self.numframes, x.shape[1]))
+        for j in range(self.numframes):
+            x_density[j, :] = np.sum(
+                np.matmul(self[j].T, x)**p, axis=0)**(1 / p)
+        return x_density
+
+
+class DiffusionWavelets(Wavelet):
+    def __init__(self, A, frames="tight", beta=None, transform_spectrum="normalized"):
         if isinstance(frames, str) and frames == "tight":
             if beta is None:
                 warnings.warn("Tight frame requested and no spectral gap supplied. "
@@ -30,37 +89,49 @@ class DiffusionWavelets(object):
                     beta = np.max(np.abs(w[1:]))
 
             self.beta = beta
-            self.numframes = int(
+            numframes = int(
                 1 + np.ceil(np.log2((-1 / np.log2(self.beta)))))
         else:
-            self.numframes = frames
+            numframes = frames
 
         N = A.shape[0]
+
+        super().__init__(N, numframes, transform_spectrum)
+
         self.T = 0.5 * (A + np.eye(N))
         W = [None for i in range(self.numframes)]
 
         W[0] = np.eye(N) - self.T
         curT = self.T
-        for i in range(self.numframes):
+        self._spectral_representation = [lambda x: 1 - x]
+        genfunc = lambda x,j: -x**2**(-1 + (j)) * (-1 + x**2**(-1 + j))
+        for j in range(self.numframes):
             nextT = np.matmul(curT, curT)
-            W[i] =  curT - nextT
+            W[j] = curT - nextT
             curT = nextT
+            self._spectral_representation.append(
+                # this is the Fourier representation of the wavelets
+                #excuse me while i MATLAB in your python
+                partial(genfunc, j=float(j)))
+
         self.W = np.concatenate(W, axis=1)
 
-    def __getitem__(self, scale):
-        toret = self.W[:, scale * 2000:(scale + 1) * 2000]
-        if toret.size>0:
-            return toret
+    def _fouriermapping(self, x, domain, direction="forward"):
+        # this holds the mapping back to the Fourier domain - the eigenvalues of this operator are different.
+        if domain == 'normalized':
+            if direction == "forward":
+                if np.min(x) < 0 and np.allclose(np.max(np.abs(x)), 1, atol=1e-5):
+                    warnings.warn(
+                        "Requested a forward fourier mapping for points already in the range of this filter")
+                    return x
+                else:
+                    return 1 - 0.5 * x
+            else:
+                return 2 - 2 * x
+        # this one only works one direction until we figure out the logic.
+        elif domain == 'diffop':
+            return 2 * (x) - 1
+        elif domain == False or domain is None:
+            return x
         else:
-            raise KeyError("Attempted to slice a frame that does not exist.")
-            
-    def transform(self, x, scale=None):
-        x = _check_and_transpose(self.N,x)
-        return np.matmul(self.W.T, x)
-
-    def power_density(self, x, p=2):
-        x = _check_and_transpose(self.N,x)
-        x_density = np.zeros((self.numframes, x.shape[1]))
-        for i in range(0, self.numframes):
-            x_density[i,:] = np.sum(np.matmul(self[i].T, x)**p,axis=0)**(1 / p)
-        return x_density
+            raise NotImplementedError("This mapping is not implemented.")
